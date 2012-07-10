2c1af28a7b74cac0f004b196b49cf3de471917ea
Fernando Ortiz <fortiz2k@gmail.com>
2012-07-09 20:03:18 -0500

    change build location to /tmp

--- yaourt-installer/install.bash
+++ yaourt-installer/install.bash
@@ -2,14 +2,20 @@
 
 wget https://aur.archlinux.org/packages/pa/package-query/package-query.tar.gz -q
 
-tar xf package-query.tar.gz
-cd package-query
-makepkg -si --noconfirm --noprogressbar
+pushd /tmp
 
-wget https://aur.archlinux.org/packages/ya/yaourt/yaourt.tar.gz -q
+  tar xf package-query.tar.gz
+  cd package-query
+  makepkg -si --noconfirm --noprogressbar
+  rm -rf package-query*
 
-tar xf yaourt.tar.gz
-cd yaourt
-makepkg -si --noconfirm --noprogressbar
+  wget https://aur.archlinux.org/packages/ya/yaourt/yaourt.tar.gz -q
+
+  tar xf yaourt.tar.gz
+  cd yaourt
+  makepkg -si --noconfirm --noprogressbar
+  rm -rf yaourt*
+
+popd
 
 echo ...done
