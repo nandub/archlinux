eead5dd07c89a0e9d8790dd69291b72207b4982c
Fernando Ortiz <fortiz2k@gmail.com>
2012-07-09 20:04:18 -0500

    add cjdns simple installer script

---
+++ cjdns-installer/install.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+yaourt --version > /dev/null
+if [ $? != 0 ]
+then
+  curl https://raw.github.com/nandub/archlinux/master/yaourt-installer/install.bash | bash
+  yaourt -S cjdns-git
+else
+  yaourt -S cjdns-git
+fi 
+
