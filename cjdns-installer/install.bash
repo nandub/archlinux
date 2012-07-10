be4a1a9c3d737bfdc9a9e00e188319e775243ff8
Fernando Ortiz <fortiz2k@gmail.com>
2012-07-10 04:29:06 -0500

    Revert "change build location to /tmp for cjdns-installer"
    This reverts commit 6c473228e6f06b85be243630c89d39f4f56b1b90.

--- cjdns-installer/install.bash
+++ cjdns-installer/install.bash
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-pushd /tmp
-
-  yaourt --version > /dev/null
-  if [ $? != 0 ]
-  then
-    curl https://raw.github.com/nandub/archlinux/master/yaourt-installer/install.bash | bash
-    yaourt -S cjdns-git
-  else
-    yaourt -S cjdns-git
-  fi 
-
-popd
+yaourt --version > /dev/null
+if [ $? != 0 ]
+then
+  curl https://raw.github.com/nandub/archlinux/master/yaourt-installer/install.bash | bash
+  yaourt -S cjdns-git
+else
+  yaourt -S cjdns-git
+fi 
 
