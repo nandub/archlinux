45359c3c168d36f4d16ef95bbbcbcd549c3c8005
Fernando Ortiz <fortiz2k@gmail.com>
2012-07-15 02:10:30 -0500

    add build essential programs

--- yaourt-installer/install.bash
+++ yaourt-installer/install.bash
@@ -2,20 +2,24 @@
 
 echo Installing Yaourt package
 
+sudo pacman -Sy cmake git fakeroot gcc make
+
 pushd /tmp
 
-  wget https://aur.archlinux.org/packages/pa/package-query/package-query.tar.gz
+  wget https://aur.archlinux.org/packages/pa/package-query/package-query.tar.gz -q
 
   tar xf package-query.tar.gz
-  cd package-query
-  makepkg -si --noconfirm --noprogressbar
+  pushd package-query
+    makepkg -si --noconfirm --noprogressbar
+  popd
   rm -rf package-query*
 
   wget https://aur.archlinux.org/packages/ya/yaourt/yaourt.tar.gz -q
 
   tar xf yaourt.tar.gz
-  cd yaourt
-  makepkg -si --noconfirm --noprogressbar
+  pushd yaourt
+    makepkg -si --noconfirm --noprogressbar
+  popd
   rm -rf yaourt*
 
 popd
